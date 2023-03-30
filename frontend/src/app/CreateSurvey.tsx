d7ea6dd715844b4cd97512c79d2068b50dac6bc6
Jason Dansie <jasondansie@gmail.com>
2023-03-30 10:58:23 +0300

    adds checkbox data to array

--- frontend/src/app/CreateSurvey.tsx
+++ frontend/src/app/CreateSurvey.tsx
@@ -4,17 +4,41 @@ import classes from './CreateSurvey.module.css'
 import axios from 'axios';
 
 interface Question {
-  id: number;
+  _id: string;
   question: string;
+}
+
+interface saveQuestions {
+
+}
+
+interface survey {
+  name: string;
+  description: string;
+}
+
+interface FormData {
+  surveyName: string;
+  description: string;
+  questions: string[];
 }
 
 const CreateSurvey: React.FC = () => {
   const [questionList, setQuestionList] = useState<Question[]>([]);
+  const [savedQuestionList, setSavedQuestionList] = useState<saveQuestions[]>([]);
+  const [survey, setSurvey] = useState<survey[]>([]);
+  const [surveyName, setSurveyName] = useState<survey[]>([]);
+  const [description, setDescription] = useState<survey[]>([]);
+  const [checkedBoxes, setCheckedBoxes] = useState<string[]>([]);
+  const [formData, setFormData] = useState<FormData>({
+    surveyName: "",
+    description: "",
+    questions: [],
+  });
 
   useEffect(() => {
     axios.get<Question[]>("http://localhost:5000/list/questions")
       .then(response => {
-        console.log(response);
         setQuestionList(response.data);
       })
       .catch(error => {
@@ -23,6 +47,38 @@ const CreateSurvey: React.FC = () => {
       });
 
   }, [setQuestionList])
+
+  const onchangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    //   setSurvey({ ...survey, [e.target.name]: e.target.value })
+    if (e.target.name === "surveyName") {
+      setSurveyName({ ...surveyName, [e.target.value]: e.target.value })
+    } else {
+      setDescription({ ...description, [e.target.value]: e.target.value })
+    }
+  }
+
+  const checkboxHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked, value, id } = event.target;
+
+    if (checked) {
+      setCheckedBoxes([...checkedBoxes, value]);
+      console.log("question list: ", questionList);
+      console.log("value: ", value);
+    } else {
+      setCheckedBoxes(checkedBoxes.filter((box) => box !== name));
+    }
+  };
+
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    console.log("checkedBoxes: ", checkedBoxes);
+    const questions = checkedBoxes.map((box) => box);
+    console.log("question: ", questions);
+    setFormData((prevFormData) => ({ ...prevFormData, questions }));
+
+    // console.log("submitted", formData);
+  }
 
 
 
@@ -33,11 +89,26 @@ const CreateSurvey: React.FC = () => {
       />
       <div className={classes.top}>
         <div className={classes.maincontent}>
-          {questionList.map((question) => (
-            <div key={question.id}>
-              <p>{question.question}</p>
+          <form action="POST" onSubmit={submitHandler}>
+            <label>
+              Survey Name:
+              <input type="text" name="surveyName" onChange={onchangeHandler} />
+            </label>
+            <label>
+              Description:
+              <input type="text" name="description" onChange={onchangeHandler} />
+            </label>
+            <div>
+              {questionList.map((question, i) => (
+                <div key={question._id}>
+                  <label htmlFor={question._id}>{question.question}</label>
+                  <input type="checkbox" name={question._id} value={question._id} onChange={checkboxHandler} />
+                </div>
+              ))}
             </div>
-          ))}
+            <button type="submit">Submit</button>
+          </form>
+
         </div>
       </div>
     </div>
